b1b93014b9f80728ad2efab8b672ec18e3aafbed
Martin Müllenhaupt <mm+git@netlair.de>
2018-10-23 18:46:11 +0200

    IceAdapterProcess: enable additional commandline arguments from config and show MessageBox for crashing adapter

--- src/connectivity/IceAdapterProcess.py
+++ src/connectivity/IceAdapterProcess.py
@@ -1,10 +1,12 @@
 from decorators import with_logger
 from PyQt5.QtCore import QProcess
 from PyQt5.QtNetwork import QTcpServer, QHostAddress
+from PyQt5.QtWidgets import QMessageBox
 import os
 import sys
 from config import Settings
 import fafpath
+from typing import List
 
 if sys.platform != 'win32':
     from distutils.spawn import find_executable
@@ -21,27 +23,30 @@ class IceAdapterProcess(object):
 
         if sys.platform == 'win32':
             exe_path = os.path.join(fafpath.get_libdir(), "ice-adapter", "faf-ice-adapter.exe")
-            args = []
         else:  # Expect it to be in PATH already
             exe_path = "faf-ice-adapter"
-            args = ["--debug-window"]
 
         self.ice_adapter_process = QProcess()
-        args += ["--id", str(player_id),
-                 "--login", player_login,
-                 "--rpc-port", str(self._rpc_server_port),
-                 "--gpgnet-port", "0",
-                 "--log-level" , "debug",
-                 "--log-directory", Settings.get('client/logs/path', type=str)]
+        args = ["--id", str(player_id),
+                "--login", player_login,
+                "--rpc-port", str(self._rpc_server_port),
+                "--gpgnet-port", "0",
+                "--log-level" , "debug",
+                "--log-directory", Settings.get('client/logs/path', type=str)]
+        if Settings.contains('iceadapter/args'):
+            args += Settings.get('iceadapter/args', "", type=str).split(" ")
+
         self._logger.debug("running ice adapter with {} {}".format(exe_path, " ".join(args)))
         self.ice_adapter_process.start(exe_path, args)
 
         # wait for the first message which usually means the ICE adapter is listening for JSONRPC connections
         if not self.ice_adapter_process.waitForStarted(5000):
             self._logger.error("error starting the ice adapter process")
+            QMessageBox.critical(None, "ICE adapter error", "The ICE adapter did not start. Please refaf.")
 
         self.ice_adapter_process.readyReadStandardOutput.connect(self.on_log_ready)
         self.ice_adapter_process.readyReadStandardError.connect(self.on_error_ready)
+        self.ice_adapter_process.finished.connect(self.on_exit)
 
     def on_log_ready(self):
         for line in str(self.ice_adapter_process.readAllStandardOutput()).splitlines():
@@ -50,6 +55,18 @@ class IceAdapterProcess(object):
     def on_error_ready(self):
         for line in str(self.ice_adapter_process.readAllStandardError()).splitlines():
             self._logger.debug("ICEERROR: " + line)
+
+    def on_exit(self, code, status):
+        if status == QProcess.CrashExit:
+            self._logger.error("the ICE crashed")
+            QMessageBox.critical(None, "ICE adapter error", "The ICE adapter crashed. Please refaf.")
+            return
+        if code != 0:
+            self._logger.error("The ICE adapter closed with error code", code)
+            QMessageBox.critical(None, "ICE adapter error", "The ICE adapter closed with error code {}. Please refaf.".format(code))
+            return
+        else:
+            self._logger.debug("The ICE adapter closed with exit code 0")
 
     def rpc_port(self):
         return self._rpc_server_port
